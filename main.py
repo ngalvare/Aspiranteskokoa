4be3a05addfcd106a859ce501c3db3584fdc98c7
chadgarc <chrisgarval1996@gmail.com>
2017-01-17 18:11:43 -0500

    restar

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
+def restar(a,b):
+    c=0
+    c=a-b
+    print (c)
 def menu():
     print ("1. vuelos")
     print ("2. metodo de compra")
     print ("3. promosiones")
     print ("4. descuentos")
-    print ("5 Contactenos")
+    print ("5. Contactenos")
 menu()
-
+restar(6,2)
