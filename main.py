e6c39221bdb64b60b12d063b36c88bbea7c5f47b
hydracrux <hydracrux@outlook.com>
2017-01-17 18:18:00 -0500

    add multi

--- main.py
+++ main.py
@@ -1,26 +1,22 @@
-<<<<<<< HEAD
+import os
+def multi(a,b):
+    c=0
+    c=a*b
+    print(c)
 def restar(a,b):
     c=0
     c=a-b
     print (c)
-=======
 def sumar(a,b):
     c=0
     c=a+b
     print(c)
->>>>>>> ca8ef80514f9982578937df166a780ec76c05c22
 def menu():
     print ("1. vuelos")
     print ("2. metodo de compra")
     print ("3. promosiones")
     print ("4. descuentos")
-<<<<<<< HEAD
-    print ("5. Contactenos")
 menu()
-restar(6,2)
-=======
-    print ("5. contactenos")
-menu() 
+restar(6,2) 
 sumar(5,3)
-
->>>>>>> ca8ef80514f9982578937df166a780ec76c05c22
+multi(5,5)
