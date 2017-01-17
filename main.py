0fd44a4602f3fcb942bbbdaaa0ec2f0961e47055
chadgarc <chrisgarval1996@gmail.com>
2017-01-17 17:57:57 -0500

    Correccion main.py

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 def menu():
-    print (¨1. vuelos¨)
-    print (¨2. metodo de compra¨)
-    print (¨3. promosiones¨)
-    print (¨4. descuentos¨)
-    print (¨5. contactenos¨)
+    print ("1. vuelos")
+    print ("2. metodo de compra")
+    print ("3. promosiones")
+    print ("4. descuentos")
+    print ("5 Contactenos")
+menu()
 
